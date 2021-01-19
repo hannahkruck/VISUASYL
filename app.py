2f14da7a5602ffec988c9184a8ec0f3f629279cf
hannahkruck <73877879+hannahkruck@users.noreply.github.com>
2021-01-19 15:21:01 +0100

    Changed favicon and page title

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import altair as alt
 from altair import Chart, X, Y, Axis, SortField, OpacityValue
 import numpy as np
 
+st.set_page_config(layout="wide", page_title='VISUASYL', page_icon = "🌍")
 st.set_page_config(layout="wide")
 ast.core.services.other.set_logging_format()
 
